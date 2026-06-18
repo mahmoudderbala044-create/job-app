9d7d93f47f41f1cf8b76bb2a52aa8690e301935d
Mahmoud <mahmoudderbala044@gmail.com>
2026-06-18 15:13:19 +0300

    feat: define web routes for job applications, dashboard, and vacancy management

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Job_ApllicationsController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Job_vacancyController;
+use Illuminate\Support\Facades\Mail;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::middleware(['auth','role:job-seeker'])->group(function () {
     Route::get('job_application',[Job_ApllicationsController::class,'index'])->name('job_applications.index');
@@ -20,6 +21,10 @@ Route::middleware(['auth','role:job-seeker'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
+
 
 });
 
